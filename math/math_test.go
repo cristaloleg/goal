c898f74c7a48d0962d055a39e7301fbf716ceb2c
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-17 16:14:34 +0200

    math: Gcd & Lcm tests (#10)

--- math/math_test.go
+++ math/math_test.go
@@ -1,6 +1,8 @@
 package mathm
 
-import "testing"
+import (
+	"testing"
+)
 
 var x int
 
@@ -29,5 +31,41 @@ func BenchmarkAbsPositive(b *testing.B) {
 func BenchmarkAbsNegative(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		x = Abs(-1)
+	}
+}
+
+func TestGcd(t *testing.T) {
+	tests := []struct {
+		a    int
+		b    int
+		want int
+	}{
+		{1, 1, 1},
+		{1836311903, 1134903170, 1}, // two big Fibonacci's numbers
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := Gcd(tt.a, tt.b); got != tt.want {
+				t.Errorf("Gcd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLcm(t *testing.T) {
+	tests := []struct {
+		a    int
+		b    int
+		want int
+	}{
+		{1, 1, 1},
+		{1836311903, 1134903170, 1}, // two big Fibonacci's numbers
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := Lcm(tt.a, tt.b); got != tt.want {
+				t.Errorf("Lcm() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
