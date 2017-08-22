d3003f707b61ab3ab16222e87e2d4633419df4cd
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-08-22 20:02:50 +0200

    Damerau-Levenshtein algorithm

--- string/string.go
+++ string/string.go
@@ -1,6 +1,9 @@
 package string
 
+import "fmt"
+
 // EditDist computes distance between strings
+// Levenshtein algorithm
 func EditDist(a, b string) int {
 	len1, len2 := len(a), len(b)
 	if len1 < len2 {
@@ -24,6 +27,48 @@ func EditDist(a, b string) int {
 		row1, row2 = row2, row1
 	}
 	return row2[len2]
+}
+
+// EditDistEx computes distance between strings
+// Damerau-Levenshtein algorithm
+func EditDistEx(a, b string) int {
+	len1, len2 := len(a), len(b)
+	if len1 == 0 {
+		return len2
+	}
+	if len2 == 0 {
+		return len1
+	}
+	if len1 < len2 {
+		return EditDistEx(b, a)
+	}
+	curr, next := 0, 0
+	row := make([]int, len2+1)
+
+	for i := 0; i < len2+1; i++ {
+		row[i] = i
+	}
+
+	for i := 0; i < len1; i++ {
+		curr = i + 1
+
+		for j := 0; j < len2; j++ {
+			cost := invBool2int(a[i] == b[j] || (i > 0 && j > 0 && a[i-1] == b[j] && a[i] == b[j-1]))
+			fmt.Printf("%v %v == %v\n", a[i], b[j], cost)
+
+			next = min(min(
+				row[j+1]+1,
+				row[j]+cost),
+				curr+1)
+
+			row[j], curr = curr, next
+		}
+		row[len2] = next
+		fmt.Printf("%v\n", row)
+	}
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	return next
 }
 
 func min(a, b int) int {
--- string/string_test.go
+++ string/string_test.go
@@ -25,6 +25,48 @@ func TestEditDist(t *testing.T) {
 	}
 }
 
+func TestEditDistEx(t *testing.T) {
+	tests := []struct {
+		a    string
+		b    string
+		want int
+	}{
+		{"dog", "bug", 2},
+		{"kitten", "sitting", 3},
+		{"saturday", "sunday", 3},
+
+		{"xca", "xabc", 2},
+		{"cax", "abcx", 2},
+		{"ca", "bc", 2},
+		{"hello", "hello", 0},
+		{"hello", "helo", 1},
+		{"hello", "jello", 1},
+		{"hello", "helol", 1},
+		{"hello", "hellol", 1},
+		{"hello", "heloll", 2},
+		{"hello", "cheese", 4},
+		{"hello", "saint", 5},
+		{"hallo", "saint", 4},
+		{"hello", "", 5},
+		{"smtih", "smith", 1},
+		{"snapple", "apple", 2},
+		{"testing", "testtn", 2},
+		{"saturday", "sunday", 3},
+		{"Saturday", "saturday", 1},
+		{"orange", "pumpkin", 7},
+		{"gifts", "profit", 5},
+		// {"ca", "ac", 1},
+		// {"ca", "abc", 2},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := EditDistEx(tt.a, tt.b); got != tt.want {
+				t.Errorf("got %v want %v :: `%v` & `%v`", got, tt.want, tt.a, tt.b)
+			}
+		})
+	}
+}
+
 func BenchmarkEditDist(t *testing.B) {
 	a, b := "1234567890123456789", "123456789987654321"
 	t.ReportAllocs()
