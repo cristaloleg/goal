4a398b17030b17fb8cb5abb4da69543070f36bab
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-09-05 17:56:13 +0200

    math: extended Euclidean algorithm

--- math/math.go
+++ math/math.go
@@ -20,3 +20,16 @@ func Gcd(a, b int) int {
 func Lcm(a, b int) int {
 	return a * Gcd(a, b) / b
 }
+
+// GcdEx is an extended Euclid algorithm
+func GcdEx(a, b int) (int, int, int) {
+	u0, v0 := 1, 0
+	u1, v1 := 0, 1
+	for b != 0 {
+		q := a / b
+		u0, u1 = u1, u0-q*u1
+		v0, v1 = v1, v0-q*v1
+		a, b = b, a%b
+	}
+	return a, u0, v0
+}
