93e8ad593d16bdea0322b9d6ed4a486901f80d73
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-02-04 12:35:45 +0100

    strings: SearchRabinKarp

--- string/string.go
+++ string/string.go
@@ -1,6 +1,9 @@
 package string
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 // EditDist computes distance between strings
 // Levenshtein algorithm
@@ -83,4 +86,48 @@ func invBool2int(b bool) int {
 		return 1
 	}
 	return 0
+}
+
+// primeRK is the prime base used in Rabin-Karp algorithm.
+const primeRK = 16777619
+
+// SearchRabinKarp search `sep` in `s` using Rabin-Karp algorithm
+// based on src/bytes/bytes.go
+func SearchRabinKarp(s, sep []byte) int {
+	hashsep, pow := hashStr(sep)
+	n := len(sep)
+	var h uint32
+	for i := 0; i < n; i++ {
+		h = h*primeRK + uint32(s[i])
+	}
+	if h == hashsep && bytes.Equal(s[:n], sep) {
+		return 0
+	}
+	for i := n; i < len(s); {
+		h *= primeRK
+		h += uint32(s[i])
+		h -= pow * uint32(s[i-n])
+		i++
+		if h == hashsep && bytes.Equal(s[i-n:i], sep) {
+			return i - n
+		}
+	}
+	return -1
+}
+
+// hashStr returns the hash and the appropriate multiplicative
+// factor for use in Rabin-Karp algorithm.
+func hashStr(sep []byte) (uint32, uint32) {
+	hash := uint32(0)
+	for i := 0; i < len(sep); i++ {
+		hash = hash*primeRK + uint32(sep[i])
+	}
+	var pow, sq uint32 = 1, primeRK
+	for i := len(sep); i > 0; i >>= 1 {
+		if i&1 != 0 {
+			pow *= sq
+		}
+		sq *= sq
+	}
+	return hash, pow
 }
